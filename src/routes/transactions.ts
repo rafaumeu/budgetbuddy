fc9cc97d5c118eb1397e2cccb5c62fa6440cf757
Rafael Dias Zendron <mmmarckos@gmail.com>
2024-11-12 09:47:37 -0300

    criando transações

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,12 +1,25 @@
 import { FastifyInstance } from 'fastify'
+import crypto from 'node:crypto'
+import { z } from 'zod'
 import { knex } from '../database'
 
 export async function transactionRoutes(app: FastifyInstance) {
-  app.get('/hello', async () => {
-    const transaction = await knex('transactions')
-    .where('amount', '>', 0)
-    .select('*')
-  
-    return transaction
+  app.post('/', async (req, reply) => {
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit'])
+    })
+
+    const {title, amount, type} = createTransactionBodySchema.parse(req.body)
+    
+    await knex('transactions')
+    .insert({
+      id: crypto.randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : amount * -1,
+    })
+
+    return reply.status(201).send()
   })
 }
