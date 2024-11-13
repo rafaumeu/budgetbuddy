789aa65392554aea8470b9d2e2d1b0661ee7af74
Rafael Dias Zendron <mmmarckos@gmail.com>
2024-11-13 09:09:11 -0300

    validando e exibindo os dados do usuário de acordo com o Id da sessão

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -2,24 +2,49 @@ import { FastifyInstance } from 'fastify'
 import crypto from 'node:crypto'
 import { z } from 'zod'
 import { knex } from '../database'
+import { checkSessionIdExists } from '../middlewares/check-session-id-exists'
 
 export async function transactionRoutes(app: FastifyInstance) {
-  app.get('/', async () => {
-    const transactions = await knex('transactions').select()
-    return { transactions }
-  })
+  app.get(
+    '/',
+    {
+      preHandler: [checkSessionIdExists],
+    },
+    async (request) => {
+      const { sessionId } = request.cookies
 
-  app.get('/:id', async (req) => {
+      const transactions = await knex('transactions')
+        .where('session_id', sessionId)
+        .select()
+
+      return { transactions }
+    },
+  )
+
+  app.get('/:id', { 
+    preHandler: [checkSessionIdExists]
+  }, 
+  async (req) => {
     const getTransactionParamsSchema = z.object({
       id: z.string().uuid(),  
     })
+    const { sessionId } = req.cookies
+    
     const { id } = getTransactionParamsSchema.parse(req.params) 
-    const transactions = await knex('transactions').where('id', id).first()
+    const transactions = await knex('transactions').where({
+      session_id: sessionId, 
+      id
+    }).first()
     return { transactions } 
   })
 
-  app.get('/summary', async () => {
+  app.get('/summary', { 
+    preHandler: [checkSessionIdExists]
+  }, 
+  async (req) => {
+    const { sessionId } = req.cookies
     const summary = await knex('transactions').sum('amount', { as: 'amount'})
+    .where('session_id', sessionId)
     return { summary }
   })
 
@@ -31,11 +56,11 @@ export async function transactionRoutes(app: FastifyInstance) {
     })
 
     const {title, amount, type} = createTransactionBodySchema.parse(req.body)
-    let sessionId = req.cookies.session_id
+    let sessionId = req.cookies.sessionId
 
     if(!sessionId) {
       sessionId = crypto.randomUUID()
-      reply.cookie('session_id', sessionId, {
+      reply.setCookie('session_id', sessionId, {
         path: '/',
         maxAge: 60 * 60 * 24 * 7 // 7 days
       })
