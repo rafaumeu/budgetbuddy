78bc933120dde7a99a95c368905f0a2162225fa2
Rafael Dias Zendron <mmmarckos@gmail.com>
2024-11-13 08:13:08 -0300

    adicionando cookies as transações criadas

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -31,12 +31,21 @@ export async function transactionRoutes(app: FastifyInstance) {
     })
 
     const {title, amount, type} = createTransactionBodySchema.parse(req.body)
-    
+    let sessionId = req.cookies.session_id
+
+    if(!sessionId) {
+      sessionId = crypto.randomUUID()
+      reply.cookie('session_id', sessionId, {
+        path: '/',
+        maxAge: 60 * 60 * 24 * 7 // 7 days
+      })
+    }
     await knex('transactions')
     .insert({
       id: crypto.randomUUID(),
       title,
       amount: type === 'credit' ? amount : amount * -1,
+      session_id: sessionId
     })
 
     return reply.status(201).send()
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
+import cookie from '@fastify/cookie'
 import fastify from 'fastify'
 import { transactionRoutes } from './routes/transactions'
 
-
 const app = fastify()
 
+app.register(cookie)
 app.register(transactionRoutes, {
   prefix: 'transactions'
 })
