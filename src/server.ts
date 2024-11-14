617921d580cc515691a745e020fc00a1311f379e
Rafael Dias Zendron <mmmarckos@gmail.com>
2024-11-14 07:32:42 -0300

    alterando o servidor para o deploy

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
-import { app } from './app'
+  import { app } from './app'
 
 
-app
-  .listen({
-    port: 3333,
-  })
-  .then(() => {
-    console.log('HTTP Server Running on http://localhost:3333')
-  })
+  app
+    .listen({
+      port: 3333,
+      host: ("RENDER" in process.env) ? '0.0.0.0' : 'localhost'
+    })
+    .then(() => {
+      console.log('HTTP Server Running on http://localhost:3333')
+    })
